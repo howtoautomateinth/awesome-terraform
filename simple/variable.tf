3a263fc7c9a4b54ffbe51b2681a707a75775b815
howtoautomateinth <mart@howtoautomate.in.th>
2020-02-05 00:29:04 +0700

    Added simple variables in terraform

---
+++ simple/variable.tf
@@ -0,0 +1,17 @@
+variable "myvar" {
+  type = "string"
+  default = "hello world"
+  description = "first variable on terraform"
+}
+
+variable "mylist" {
+  type = list(string)
+  default = [1,2,3]
+}
+
+variable "mymap" {
+  type = map(string)
+  default = {
+      mykey = "my value"
+  }
+}
