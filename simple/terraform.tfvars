cd5994c555a4528a1b9a1b0c428067d3a9c3b952
howtoautomateinth <mart@howtoautomate.in.th>
2020-02-05 23:42:15 +0700

    Added example variable definitions files

---
+++ simple/terraform.tfvars
@@ -0,0 +1,3 @@
+AWS_ACCESS_KEY="key123"
+AWS_SECRET_KEY="secret123"
+AWS_REGION="us-east-1"
--- simple/variable.tf
+++ simple/variable.tf
@@ -15,3 +15,15 @@ variable "mymap" {
       mykey = "my value"
   }
 }
+
+variable "AWS_ACCESS_KEY" {
+  
+}
+
+variable "AWS_SECRET_KEY" {
+  
+}
+
+variable "AWS_REGION" {
+  default = "eu-west-1"
+}
