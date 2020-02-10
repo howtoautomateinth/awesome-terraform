5e0f7a689a7b387885b5661a04f8b9179eea9164
howtoautomateinth <mart@howtoautomate.in.th>
2020-02-11 00:19:15 +0700

    Added  ECR example

---
+++ aws/ecr/ecr.tf
@@ -0,0 +1,4 @@
+resource "aws_ecr_repository" "hta_simpleapp" {
+  name = "hta_simpleapp"
+}
+
---
+++ aws/ecr/output.tf
@@ -0,0 +1,4 @@
+output "hta-repository" {
+  value = aws_ecr_repository.hta_simpleapp.repository_url
+}
+
